5db7bd928060713e7d68cb02fcbc684d6094f800
Magdalena Zakrzewska <magdazakrzewskamagda@gmaill.com>
2024-04-07 14:34:14 +0200

    chore: add file

--- Main.java
+++ Main.java
@@ -1,41 +1,8 @@
-
-class Point {
-    private double x;
-    private double y;
-
-   
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-   
-    public void setX(double x) {
-        this.x = x;
-    }
-
- 
-    public void setY(double y) {
-        this.y = y;
-    }
-}
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-       
-        Point point = new Point(3.5, 2.0);
-
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x = " + point.getX());
-        System.out.println("y = " + point.getY());
+        Point point = new Point(10,10);
+        System.out.printf("Point coords: x:%s : y:%s%n",point.getX(), point.getY());
     }
 }
